a152583310ac093bb860d07d838bcd21acd9020c
Dominik Burgdörfer <dominik.burgdoerfer@mikanda.de>
2013-07-12 17:42:43 +0200

    updated example

--- example.js
+++ example.js
@@ -8,20 +8,67 @@ var query = require('query')
   , dynamicRows = require('dynamic-rows');
 
 var template = domify([
-  '<tr>',
+  '<tr data-index="{index}">',
   '  <td>',
   '    <span>At {index}</span>',
   '    <input type="text">',
+  '    <input class="add" type="button" value="+">',
+  '    <input class="remove" type="button" value="-">',
   '  </td>',
   '</tr>'
 ].join('\n'));
 var rows = dynamicRows(query('#rows'), template);
-rows.append();
+
+/**
+ * Register click events for buttons
+ */
+
+// add new entry
+
 query('#add').onclick = function(){
-  var index = query('#indexValue').value;
-  rows.append(parseInt(index));
+  var index = parseInt(query('#indexValue').value)
+    , el;
+  el = rows.append(index);
+  initWidget(el);
 };
+
+// remove entry
+
 query('#remove').onclick = function(){
   var index = parseInt(query('#indexValue').value);
+  if (Number.isNaN(index)) return;
   rows.remove(index);
 };
+
+/**
+ * Bind the 'remove' and 'add' events of a widget.
+ */
+
+function initWidget(el) {
+
+  // add button
+
+  query('.add', el).onclick = function(){
+
+    // the index needs to be recalculated since it could have been changed
+
+    var index = dom(el).attr('data-index');
+
+    // append after the current element
+
+    initWidget(rows.append(index));
+  };
+
+  // remove button
+
+  query('.remove', el).onclick = function(){
+
+    // the index needs to be recalculated since it could have been changed
+
+    var index = dom(el).attr('data-index');
+
+    // remove the current element
+
+    rows.remove(index);
+  };
+}
